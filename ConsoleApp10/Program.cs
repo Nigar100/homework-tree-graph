c4a6c201be75850e49c93f99608495f4f79987f2
Nigar100 <nigargba@code.edu.az>
2025-09-15 00:51:23 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,114 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region FirstTask
+
+//int[] arr = { 7, 5, 12, 5, 8 };
+
+//int minIndex = Array.IndexOf(arr, arr.Min());
+
+//int[] result = new int[arr.Length - 1];
+
+//int j = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (i == minIndex) continue; 
+//    result[j++] = arr[i];
+//}
+
+//Console.WriteLine("netice: {" + string.Join(", ", result) + "}");
+
+
+#endregion
+
+#region SecondTask
+
+
+// string text = "kertenleke";
+//char result = '\0';
+
+
+//        for (int i = 0; i < text.Length; i++)
+//        {
+//            char current = text[i];
+//            int count = 0;
+
+
+//            for (int j = 0; j < text.Length; j++)
+//            {
+//                if (text[j] == current)
+//                    count++;
+//            }
+
+
+//            if (count == 1)
+//            {
+//                result = current;
+//                break;
+//            }
+//        }
+
+//        if (result != '\0')
+
+//            Console.WriteLine("netice: " + result);
+//        else
+//            Console.WriteLine("Yalniz 1 dəfə islenmis simvol tapilmadi.");
+#endregion
+
+#region ThirdTask
+//        int n = int.Parse(Console.ReadLine());
+
+//int[]arr = {30,35,67,77};
+
+//if (n <= 1)
+//        {
+//            Console.WriteLine("0 və 1 sade ve ya murekkeb deyil.");
+//            return;
+//        }
+
+//        bool sade = true; 
+
+
+//        for (int i = 2; i <= Math.Sqrt(n); i++) 
+//        {
+//            if (n % i == 0)
+//            {
+//                sade = false;
+//                break;
+//            }
+//        }
+
+
+//        if (sade)
+//            Console.WriteLine($"{n} sade ededdir.");
+//else
+//    Console.WriteLine($"{n}  murekkeb ededdir.");
+
+#endregion
+
+#region FourthTask
+
+
+  
+     //string text = "salam necesiz".Replace(" ", "");
+     //   int[] m = new int[char.MaxValue];
+
+     //   // Sayma
+     //   foreach (char c in text)
+     //       m[c]++;
+
+     //   char maxChar = '\0';
+     //   int maxCount = 0;
+
+     //   foreach (char c in text)
+     //   {
+     //       if (m[c] > maxCount)
+     //       {
+     //           maxCount = m[c];
+     //           maxChar = c;
+     //       }
+     //   }
+
+     //   Console.WriteLine($" '{maxChar}', say: {maxCount}");
+    
+
+#endregion
